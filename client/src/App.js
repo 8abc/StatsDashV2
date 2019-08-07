237d3a9f3afcf9adbc50099ef7ab0b9095abe9ee
Anadelaine Catalla <46228172+8abc@users.noreply.github.com>
2019-08-07 11:41:21 -0700

    Revert "created navbar with routes and basic jumbotron"

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
+
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
+
 import NavBar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
+
 import "./App.css";
 
 // Check for token to keep user logged in
@@ -31,7 +34,6 @@ if (localStorage.jwtToken) {
     window.location.href = "./login";
   }
 }
-
 
 class App extends Component {
   render() {
--- client/src/pages/Dashboard/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Dashboard extends Component {
-  render() {
-    return <div>Dashboard Compare Mode</div>;
-  }
-}
-
-export default Dashboard;
