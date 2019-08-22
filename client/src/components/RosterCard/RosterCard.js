eb781023feefa466559340cd820dbab4eea22f19
8abc <anadelaine.catalla@gmail.com>
2019-08-21 20:22:03 -0700

    created roster card, state, handle change and player search function

--- client/src/components/RosterCard/RosterCard.js
+++ client/src/components/RosterCard/RosterCard.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card, CardTitle, Row, Col } from "react-materialize";
+import "./style.css";
 
 class RosterCard extends Component {
   render() {
+    console.log("rostercard props: ", this.props);
+    const playerData = () => (
+      <div>
+        <ul>
+          <p>Team: {this.props.playerInfo.team}</p>
+          <p>2-Points Made: {this.props.playerInfo.two_points_made}</p>
+          <p>3-Points Made: {this.props.playerInfo.three_points_made}</p>
+          <p>Free Throws Made: {this.props.playerInfo.free_throws_made}</p>
+          <p>Rebounds: {this.props.playerInfo.rebounds}</p>
+          <p>Assist: {this.props.playerInfo.assists}</p>
+          <p>Blocks: {this.props.playerInfo.blocks}</p>
+        </ul>
+      </div>
+    );
     return (
       <Row>
-        <Col m={6} s={12}>
-          <Card header={<CardTitle />} title="Card Title" reveal={<p />}>
+        <Col s={3}>
+          <Card
+            className="grey darken-4"
+            id="rosterCard"
+            header={<CardTitle />}
+            title={this.props.playerInfo.fullname}
+            reveal={<p>{playerData()}</p>}
+          >
             <p>
-              <a href="#">This is a link</a>
+              <a className="waves-effect waves-light btn-x small">
+                <i className="material-icons left">close</i>
+              </a>
             </p>
           </Card>
         </Col>
