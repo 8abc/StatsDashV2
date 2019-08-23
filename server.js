ac46746697fa95aac9d903d869cf63670f8b01f6
The Becs <beccyvalentine@gmail.com>
2019-08-22 19:29:52 -0700

    change to the uri for mongoDB

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const search = require("./routes/api/search");
 const app = express();
 // Bodyparser middleware
 app.use(
@@ -24,5 +25,6 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+app.use("/api/search", search);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
